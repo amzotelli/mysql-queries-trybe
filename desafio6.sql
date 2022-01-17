e16105da642ee805f02262e4f90cd0f21529711b
Amanda Zotelli <77364052+amzotelli@users.noreply.github.com>
2022-01-17 16:09:07 -0300

    Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT 
-MIN(planos.plano_valor) AS 'faturamento_minimo',
-MAX(planos.plano_valor) AS 'faturamento_maximo',
-ROUND(AVG(planos.plano_valor), 2) AS 'faturamento_medio',
-ROUND(SUM(planos.plano_valor), 2) AS 'faturamento_total'
+MIN(planos.plano_valor) AS faturamento_minimo,
+MAX(planos.plano_valor) AS faturamento_maximo,
+ROUND(AVG(planos.plano_valor), 2) AS faturamento_medio,
+ROUND(SUM(planos.plano_valor), 2) AS faturamento_total
 FROM planos
 INNER JOIN usuario ON planos.plano_id = usuario.plano_id; 
