f37ce7d656ea095588be5bf1a9324ffce84e9702
Amanda Zotelli <amzotelli@gmail.com>
2022-01-17 18:16:28 -0300

    remove query duplicada requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,3 @@
 SELECT COUNT(*) AS "quantidade_musicas_no_historico" FROM historico_reproducao
 JOIN usuario ON historico_reproducao.usuario_id = usuario.usuario_id
 WHERE usuario.nome = "Bill";
-SELECT COUNT(*) AS "quantidade_musicas_no_historico" FROM historico_reproducao
-JOIN usuario ON historico_reproducao.usuario_id = usuario.usuario_id
-WHERE usuario.nome = "Bill";
